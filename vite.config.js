779f533db327cb4188ecd5c99ed4ec44b544ed02
GayathriHG <gai388926@gmail.com>
2023-10-15 19:40:14 +0530

    added menu, contact, about

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base:"/KAAPI_MANE-skillanalyse/"
+  base:"/KAAPI_MANE-skillanalyse/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about-us.html'),
+        menu: resolve(__dirname, 'menu.html'),
+        contact: resolve(__dirname, 'contact-us.html'),
+      }
+    }
+  }
 })
